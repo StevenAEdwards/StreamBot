ab6b774709a5853bff9759b373d50ea9ea1c5bba
StevenEdwards <steven@lepto.net>
2024-09-29 09:11:45 -0400

    fixing whitespace

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Client, StageChannel } = require('discord.js-selfbot-v13');
 const { command, streamLivestreamVideo, getInputMetadata, inputHasAudio, Streamer } = require('@dank074/discord-video-stream');
 //API
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`API server is listening on port ${port}`);
@@ -17,65 +17,65 @@ streamer.client.on('ready', () => {
 
 app.post('/play', async (req, res) => {
     const { guildId, channelId, streamURL, qualities } = req.body;
-  
+
     if (!guildId || !channelId || !streamURL) {
-      return res.status(400).send('Missing required parameters: guildId, channelId, streamURL');
+        return res.status(400).send('Missing required parameters: guildId, channelId, streamURL');
     }
-  
+
     try {
-      const guild = streamer.client.guilds.cache.get(guildId);
-      if (!guild) return res.status(404).send('Guild not found.');
-  
-      const channel = guild.channels.cache.get(channelId);
-      if (!channel || channel.type !== 'GUILD_VOICE') {
-        return res.status(404).send('Voice channel not found or invalid.');
-      }
-  
-      const currentVoiceState = streamer.client.user.voice;
-  
-      if (currentVoiceState && currentVoiceState.channelId === channelId) {
-        console.log(`Already connected to voice channel ${guildId}/${channelId}`);
-      } else {
-        console.log(`Joining voice channel ${guildId}/${channelId}`);
-        await streamer.joinVoice(guildId, channelId);
-  
-        if (channel instanceof StageChannel) {
-          await streamer.client.user.voice.setSuppressed(false);
+        const guild = streamer.client.guilds.cache.get(guildId);
+        if (!guild) return res.status(404).send('Guild not found.');
+
+        const channel = guild.channels.cache.get(channelId);
+        if (!channel || channel.type !== 'GUILD_VOICE') {
+            return res.status(404).send('Voice channel not found or invalid.');
         }
-      }
-  
-      let metadata;
-      try {
-        metadata = await getInputMetadata(streamURL);
-      } catch (e) {
-        console.log('Error fetching metadata:', e);
-        return res.status(500).send('Failed to fetch stream metadata.');
-      }
-  
-      try {
-        await killAllFfmpegProcesses();
-  
-        if (currentVoiceState && currentVoiceState.streaming) {
-          console.log('Already streaming, switching streams...');
-          switchStreams(streamURL, metadata);
+
+        const currentVoiceState = streamer.client.user.voice;
+
+        if (currentVoiceState && currentVoiceState.channelId === channelId) {
+            console.log(`Already connected to voice channel ${guildId}/${channelId}`);
         } else {
-          console.log('No active stream, starting new stream...');
-          const streamUdpConn = await streamer.createStream(generateStreamOptions(qualities, metadata));
-          playVideo(streamURL, metadata, streamUdpConn);
+            console.log(`Joining voice channel ${guildId}/${channelId}`);
+            await streamer.joinVoice(guildId, channelId);
+
+            if (channel instanceof StageChannel) {
+                await streamer.client.user.voice.setSuppressed(false);
+            }
         }
-  
-        await new Promise(resolve => setTimeout(resolve, 4000));
-  
-        return res.status(200).send('Streaming started successfully.');
-      } catch (streamError) {
-        console.error('Error while streaming:', streamError);
-        return res.status(500).send('Failed to start streaming.');
-      }
-    } catch (error) {  
-      console.error('Unexpected error while processing the /play request:', error);
-      return res.status(500).send('Failed to process the /play request.');
+
+        let metadata;
+        try {
+            metadata = await getInputMetadata(streamURL);
+        } catch (e) {
+            console.log('Error fetching metadata:', e);
+            return res.status(500).send('Failed to fetch stream metadata.');
+        }
+
+        try {
+            await killAllFfmpegProcesses();
+
+            if (currentVoiceState && currentVoiceState.streaming) {
+                console.log('Already streaming, switching streams...');
+                switchStreams(streamURL, metadata);
+            } else {
+                console.log('No active stream, starting new stream...');
+                const streamUdpConn = await streamer.createStream(generateStreamOptions(qualities, metadata));
+                playVideo(streamURL, metadata, streamUdpConn);
+            }
+
+            await new Promise(resolve => setTimeout(resolve, 4000));
+
+            return res.status(200).send('Streaming started successfully.');
+        } catch (streamError) {
+            console.error('Error while streaming:', streamError);
+            return res.status(500).send('Failed to start streaming.');
+        }
+    } catch (error) {
+        console.error('Unexpected error while processing the /play request:', error);
+        return res.status(500).send('Failed to process the /play request.');
     }
-  });
+});
 
 app.post('/disconnect', async (req, res) => {
     try {
@@ -180,69 +180,69 @@ function generateStreamOptions(qualities, metadata) {
 
     const frameRateParts = videoStream.avg_frame_rate.split('/');
     const inputFps = frameRateParts.length === 2 ? parseInt(frameRateParts[0], 10) / parseInt(frameRateParts[1], 10) : parseFloat(videoStream.avg_frame_rate);
-    
+
     const inputHeight = videoStream.height;
     const inputWidth = videoStream.width;
 
-    const height = process.env.HEIGHT ? parseInt(process.env.HEIGHT, 10) 
-                 : qualities?.height ? qualities.height 
-                 : inputHeight;
-                 
-    const width = process.env.WIDTH ? parseInt(process.env.WIDTH, 10) 
-                : qualities?.width ? qualities.width 
-                : inputWidth;
-                
-    const fps = process.env.FPS ? parseInt(process.env.FPS, 10) 
-              : qualities?.fps ? qualities.fps 
-              : Math.round(inputFps);
+    const height = process.env.HEIGHT ? parseInt(process.env.HEIGHT, 10)
+        : qualities?.height ? qualities.height
+            : inputHeight;
+
+    const width = process.env.WIDTH ? parseInt(process.env.WIDTH, 10)
+        : qualities?.width ? qualities.width
+            : inputWidth;
+
+    const fps = process.env.FPS ? parseInt(process.env.FPS, 10)
+        : qualities?.fps ? qualities.fps
+            : Math.round(inputFps);
 
     let defaultBitrateKbps, defaultMaxBitrateKbps;
-    if (fps >= 45) { 
+    if (fps >= 45) {
         if (height >= 1080) {
-            defaultBitrateKbps = 6000;  
+            defaultBitrateKbps = 6000;
             defaultMaxBitrateKbps = 9000;
         } else if (height >= 720) {
-            defaultBitrateKbps = 4000;  
+            defaultBitrateKbps = 4000;
             defaultMaxBitrateKbps = 6000;
         } else {
-            defaultBitrateKbps = 2500;  
+            defaultBitrateKbps = 2500;
             defaultMaxBitrateKbps = 3500;
         }
-    } else if (fps >= 15) { 
+    } else if (fps >= 15) {
         if (height >= 1080) {
-            defaultBitrateKbps = 5000;  
+            defaultBitrateKbps = 5000;
             defaultMaxBitrateKbps = 7000;
         } else if (height >= 720) {
-            defaultBitrateKbps = 3000;  
+            defaultBitrateKbps = 3000;
             defaultMaxBitrateKbps = 4500;
         } else {
-            defaultBitrateKbps = 2000;  
+            defaultBitrateKbps = 2000;
             defaultMaxBitrateKbps = 3000;
         }
     } else {
         if (height >= 1080) {
-            defaultBitrateKbps = 4000;  
+            defaultBitrateKbps = 4000;
             defaultMaxBitrateKbps = 6000;
         } else if (height >= 720) {
-            defaultBitrateKbps = 2500;  
+            defaultBitrateKbps = 2500;
             defaultMaxBitrateKbps = 3500;
         } else {
-            defaultBitrateKbps = 1500;   
+            defaultBitrateKbps = 1500;
             defaultMaxBitrateKbps = 2000;
         }
     }
 
-    const bitrateKbps = process.env.BITRATE_KBPS ? parseInt(process.env.BITRATE_KBPS, 10) 
-                        : qualities?.bitrateKbps ? qualities.bitrateKbps 
-                        : defaultBitrateKbps;
-    const maxBitrateKbps = process.env.MAX_BITRATE_KBPS ? parseInt(process.env.MAX_BITRATE_KBPS, 10) 
-                           : qualities?.maxBitrateKbps ? qualities.maxBitrateKbps 
-                           : defaultMaxBitrateKbps;
-    const h26xPreset = process.env.H26X_PRESET 
-                      ? process.env.H26X_PRESET 
-                      : qualities?.h26xPreset 
-                      ? qualities.h26xPreset 
-                      : "superfast";
+    const bitrateKbps = process.env.BITRATE_KBPS ? parseInt(process.env.BITRATE_KBPS, 10)
+        : qualities?.bitrateKbps ? qualities.bitrateKbps
+            : defaultBitrateKbps;
+    const maxBitrateKbps = process.env.MAX_BITRATE_KBPS ? parseInt(process.env.MAX_BITRATE_KBPS, 10)
+        : qualities?.maxBitrateKbps ? qualities.maxBitrateKbps
+            : defaultMaxBitrateKbps;
+    const h26xPreset = process.env.H26X_PRESET
+        ? process.env.H26X_PRESET
+        : qualities?.h26xPreset
+            ? qualities.h26xPreset
+            : "superfast";
 
     const readAtNativeFps = process.env.READ_AT_NATIVE_FPS || qualities?.readAtNativeFps || 'true';
     const hardwareAcceleratedDecoding = process.env.HARDWARE_ACCELERATION === 'true' || qualities?.hwAccel === 'true';
